2421fdea9a5a263acd0f32080a4a8ae48176453d
abui6 <abui6@uncc.edu>
2016-10-13 02:38:15 +0000

    Done with section 4.10

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,49 +1,72 @@
-# This file is app/controllers/movies_controller.rb
 class MoviesController < ApplicationController
+  
   def index
     @movies = Movie.all
   end
   
-   def show 
-   id = params[:id] # retrieve movie ID from URI route 
-   @movie = Movie.find(id) # look up movie by unique ID 
-   # will render app/views/movies/show.html.haml by default 
-   end 
-   
-  def new
-  @movie = Movie.new
-  # default: render 'new' template
-  end 
-  
-  private
-  def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  def show
+  id = params[:id] # retrieve movie ID from URI route
+  @movie = Movie.find(id) # look up movie by unique ID
+  # will render app/views/movies/show.html.haml by default
   end
   
-   def create
+  def create
     #@movie = Movie.create!(params[:movie]) #old way
     @movie = Movie.create!(movie_params)  # new way
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
-    end
-    
-    def destroy
-  @movie = Movie.find(params[:id])
-  @movie.destroy
-  flash[:notice] = "Movie '#{@movie.title}' deleted."
-  redirect_to movies_path
-end
+  end
 
-def edit
-  @movie = Movie.find params[:id]
-end
-
-
-     def update
+  def new
+  @movie = Movie.new
+  # default: render 'new' template
+  end 
+  def update
     @movie = Movie.find params[:id]
     #@movie.update_attributes!(params[:movie])  # old way
     @movie.update_attributes!(movie_params)  # new way  
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
-    end
+  end
+  
+  # in movies_controller.rb
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+  def update
+  @movie = Movie.find params[:id]
+  @movie.update_attributes!(params[:movie])
+  respond_to do |client_wants|
+    client_wants.html {  redirect_to movie_path(@movie)  } # as before
+    client_wants.xml  {  render :xml => @movie.to_xml    }
+  end
+  end
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+  
+  def update
+    @movie = Movie.find params[:id]
+    #@movie.update_attributes!(params[:movie])  # old way
+    @movie.update_attributes!(movie_params)  # new way  
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+  
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
+
 end
